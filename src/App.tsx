124783d71aeda597f8777193c91534cee2bc6108
CheBar1 <chebarclay@live.com.au>
2023-03-21 18:00:55 +1030

    playing with CSS

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,19 @@ import { useState } from "react";
 import Alert from "./components/Alert";
 import Button from "./components/Button";
 
+
 function App() {
   const [alertVisible, setAlertVisibility] = useState(false);
 
   return (
     <div>
-      {alertVisible && (
+       {alertVisible && (
         <Alert onClose={() => setAlertVisibility(false)}>My Alert</Alert>
       )}
       <Button color="primary" onClick={() => setAlertVisibility(true)}>
         My Button
-      </Button>
+       </Button>
+   
     </div>
   );
 }
